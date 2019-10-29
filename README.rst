04a4b37c8afd9b8050598097d98b1e2636eb223e
Deemo <xxb1021054331@gmail.com>
2019-10-29 10:36:13 +0800

    打包aiospider库，可以安装在任何地方使用

---
+++ README.rst
@@ -0,0 +1,1 @@
+# 一个基于 asyncio 和 aiohttp 的异步爬虫框架
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# time    : 2019/10/25 5:18 下午
+
+
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="AioSpider",
+    version="0.2.6",
+    author="yueyue",
+    author_email="xxb1021054331@gmail.com",
+    description="a crawl framework based on asyncio and aiohttp",
+    long_description=open("README.rst").read(),
+    packages=find_packages(include=('aiospider')),
+    zip_safe=True,
+    install_requires=[
+        'ujson>=1.35',
+        'cchardet>=2.1.4',
+        'typing>=3.7.4',
+        'redis>=3.3.10',
+        'pydantic>=0.29',
+        'Faker>=2.0.2',
+        'uvloop>=0.13.0',
+        'setuptools>=41.0.1',
+        'aiohttp>=3.5.4',
+        'pyquery>=1.4.0',
+    ]
+)
